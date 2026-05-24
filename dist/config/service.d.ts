dad25c02784db8c95b0c548f41ca68dcb684ba08
Nate Williams <nate@yogotech.com>
2026-05-24 12:10:33 -0600

    Update gitignore - Never commit generated files to a repo

--- dist/config/service.d.ts
+++
@@ -1,8 +0,0 @@
-export interface Service {
-    domain: string;
-    service: string;
-    data?: {
-        [key: string]: string;
-    };
-}
-export default function parseServie(config: false | Service): Service;
--- dist/getEntityType.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const DUAL = "dual";
-export declare const SINGLE = "single";
-export default function getEntityType(attributes: any): "dual" | "single";
